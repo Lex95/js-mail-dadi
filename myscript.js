a690c71dbafdd005434ecb17298a71b6329f68db
Lex95 <andrea.lega1995@gmail.com>
2021-04-09 14:48:31 +0200

    aggiunta gioco dei dadi

--- myscript.js
+++ myscript.js
@@ -10,9 +10,22 @@ for (var i = 0; i < mailWhiteList.length; i++) {
     }
 }
 
+var dadoUtente = Math.floor(Math.random() * 6) + 1;
+var dadoIa = Math.floor(Math.random() * 6) + 1;
+
 if (isAllowed == true) {
     alert("La mail è corretta!");
+    if (dadoUtente > dadoIa) {
+        document.getElementById("myId").innerHTML= "L'utente " + mailUtente + " ha vinto contro il computer " + dadoUtente + " a " + dadoIa + "!";
+    }
+    else if (dadoUtente < dadoIa) {
+        document.getElementById("myId").innerHTML= "L'utente " + mailUtente + " ha perso contro il computer " + dadoUtente + " a " + dadoIa + "!";
+    }
+    else {
+        document.getElementById("myId").innerHTML= "L'utente " + mailUtente + " e il computer hanno pareggiato con " + dadoUtente + " e " + dadoIa + "!";
+    }
 }
 else {
     alert("La mail non è nella lista!");
+    document.getElementById("myId").innerHTML= "Non hai il permesso per poter giocare!";
 }
